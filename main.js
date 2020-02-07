79319b3f5dd1f9545dbae83d6afaa7a70f90beb5
saud-cloud <59025948+saud-cloud@users.noreply.github.com>
2020-02-07 10:14:29 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
 // load the http module
 var http = require('http');
 
-// configure our HTTP server
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello NodeApp!! the following webapp is being created by saud \n");
-});
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const path = __dirname + '/views/';
+const port = 8000;
 
 // listen on localhost:8000
-server.listen(8000);
-console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
+//server.listen(8000);
+//console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
+
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function(req,res){
+  res.sendFile(path + 'sharks.html');
+});
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!')
+})
+
