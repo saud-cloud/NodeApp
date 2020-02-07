80b30342eedefbafe4297a3d7a92acebf07c0879
saud-cloud <59025948+saud-cloud@users.noreply.github.com>
2020-02-07 16:41:19 +0500

    Update main.js

--- main.js
+++ main.js
@@ -19,6 +19,8 @@ var path = require('path');
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+    res.send(‘hello world’);
 });
 
 app.listen(8000);
+module.exports = app;
