190b52ed9419d077624ac0dac1f79b4f484d7b3e
saud-cloud <59025948+saud-cloud@users.noreply.github.com>
2020-02-07 16:56:58 +0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
-var assert = require('assert')
+/*var assert = require('assert')
 
 function test() {
   assert.equal(2 + 2, 4);
 }
 
 if (module == require.main) require('test').run(test);
+*/
+var request = require(‘supertest’);
+var app = require(‘__dirname/index.js’);
+describe(‘GET /’, function() {
+ it(‘respond with hello world’, function(done) {
+ //navigate to root and check the the response is "hello world"
+ request(app).get(‘/’).expect(‘hello world’, done);
+ });
+});
