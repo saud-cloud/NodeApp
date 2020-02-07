fc92bda5fbbffa6fb1214908c1c932db37bd1d3a
saud-cloud <59025948+saud-cloud@users.noreply.github.com>
2020-02-07 11:35:10 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // load the http module
+/*
 var http = require('http');
 
 // configure our HTTP server
@@ -10,3 +11,14 @@ var server = http.createServer(function (request, response) {
 // listen on localhost:8000
 server.listen(8000);
 console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
+*/
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
